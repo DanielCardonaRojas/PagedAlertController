6efb55b2f99d1448254852d891b31d5e1911dcfd
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2016-08-22 15:18:45 -0500

    Initial Commit

---
+++ PagedAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PagedAlertController
+//
+//  Created by Daniel Cardona Rojas on 8/22/16.
+//  Copyright © 2016 Daniel Cardona Rojas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
